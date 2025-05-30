339ab788f04e1bcae83fb4b13810cec8149ce9ac
bishowlamichhane <lamichhanebisho4545@gmail.com>
2025-05-30 18:03:00 +0545

    framer;

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,18 +4,30 @@ import HeaderComponent from "../components/HeaderComponent";
 import FetchItems from "../components/FetchItems";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  //ignore
+  const [isVisible, setIsVisible] = useState(false);
   const fetchStatus = useSelector((store) => store.fetchStatus);
   const items = useSelector((store) => store.items);
+
+  useEffect(() => {
+    // Set isVisible to true when component mounts
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isVisible ? 1 : 0 }}
+      transition={{ duration: 1.0 }}
+    >
       <HeaderComponent items={items} />
       <FetchItems />
       {fetchStatus.currentlyFetching ? <LoadingSpinner /> : <Outlet />}
       <FooterComponent />
-    </div>
+    </motion.div>
   );
 };
 
